1f0b457a27f442ab3a5cad6e372fec4f8659aa9a
Daniella Achoki <daniellaachoki@gmail.com>
2026-07-14 21:32:14 +0300

    Fetch and display posts with async await

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+async function fetchPosts(){
+const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+const posts = await response.json();
+displayPosts(posts);
+}
+function displayPosts(posts){
+    const ul = document.getElementById("post-list");
+    posts.forEach((post) => {
+        const li = document.createElement("li");
+        const h1 = document.createElement("h1");
+        h1.textContent = post.title;
+        const p = document.createElement("p");
+        p.textContent = post.body
+        li.append(h1)
+        li.append(p)
+        ul.append(li);
+
+    });
+}
+fetchPosts();
